3797e1b44d9a1451955689592c74a86d99ff0cf3
ctianlong <chengtl0131@gmail.com>
2018-06-23 14:50:26 +0800

    ignore response validate

--- src/main/java/dk/itst/oiosaml/sp/service/SAMLAssertionConsumerHandler.java
+++ src/main/java/dk/itst/oiosaml/sp/service/SAMLAssertionConsumerHandler.java
@@ -134,7 +134,7 @@ public class SAMLAssertionConsumerHandler implements SAMLHandler {
 		response.decryptAssertion(ctx.getCredential(), !ctx.getConfiguration().getBoolean(Constants.PROP_REQUIRE_ENCRYPTION, false));
 		
 		// 华为云上docker部署验证不过，docker为centos7 Oracle jdk 8u171时可以正常
-		response.validateResponse(ctx.getSpMetadata().getAssertionConsumerServiceLocation(0), metadata.getValidCertificates(), allowPassive);
+//		response.validateResponse(ctx.getSpMetadata().getAssertionConsumerServiceLocation(0), metadata.getValidCertificates(), allowPassive);
 		
 		if (allowPassive && response.isPassive()) {
 			log.debug("Received passive response, setting passive userassertion");
